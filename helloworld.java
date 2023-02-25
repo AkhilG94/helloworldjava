5f1a6f9ef10de6ed93e33a83794e49ffd50f42d1
AkhilG94 <124521559+AkhilG94@users.noreply.github.com>
2023-02-25 01:43:01 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 21st 2023 - 13:57AM - triggered by webhook");
+    System.out.println("Hello World - Feb 25th 2023 - 01:42AM - poll SCM ");
     }
 }
